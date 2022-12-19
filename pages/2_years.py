9c83f693727d7eac88a0b4c8b35f61dd0d68d1e3
jodiambra <jodiambra@gmail.com>
2022-12-19 15:27:35 -0500

    first commits

---
+++ pages/2_years.py
@@ -0,0 +1,29 @@
+#import packages
+import streamlit as st
+import pandas as pd
+import plotly_express as px
+from PIL import Image
+from streamlit.commands.page_config import Layout
+
+
+#----------------------------#
+# Upgrade streamlit library
+# pip install --upgrade streamlit
+
+#-----------------------------#
+# Page layout
+
+st.set_page_config(page_title='Spotify Over the Decade',
+                   page_icon=None,
+                   layout='wide',
+                   initial_sidebar_state="auto",
+                   menu_items=None)
+#-----------------------------#
+
+
+st.title('Spotify Over the Decade')
+# Title Picture
+
+# image_1 = Image.open('streaming-illustration-v-2019-billboard-1548.webp')
+
+# st.image(image_1, width=1000)
